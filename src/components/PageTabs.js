7dbfb16a0604ad4ba1316a6363322e5e4eb26ad8
radiounit <maria.permala@gmail.com>
2020-04-03 12:21:45 +0300

    Removed unused components

--- src/components/PageTabs.js
+++ src/components/PageTabs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import AppBar from '@material-ui/core/AppBar';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function PageTabs() {
 
